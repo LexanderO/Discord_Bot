6f4b539e18132d6fd7fef857e82b292fa272cab5
LexanderO <alexanderowens1993@gmail.com>
2018-12-07 14:46:16 +0000

    Corrected spelling error in main.js (Client.on). Updated start script in package.json

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const Discord = require ('discord.js')
 const client = new Discord.Client()
 
-clinet.on('ready', () => {
+client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
 })
 
 //insert token key 
-client.login("xxxx")
+client.login("XXXX")
