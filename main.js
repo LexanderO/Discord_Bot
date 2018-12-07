68a5b62f53f56cadbce06734abc11bcd3a7e1cd8
LexanderO <alexanderowens1993@gmail.com>
2018-12-07 15:38:26 +0000

    created config.js to separate tokens from code.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const Discord = require ('discord.js')
-const client = new Discord.Client()
+const Discord = require ('discord.js');
+const client = new Discord.Client();
+const {d_token} = require ('./config.json');
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
-})
+});
 
-//insert token key 
-client.login("XXXX")
+client.login(d_token);
